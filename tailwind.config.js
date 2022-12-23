e30ffd3106698cf84065cf42585f2d8d5f5d5ebc
Dídac Ortega Escalera <didacoe@gmail.com>
2022-12-23 16:31:41 +0100

    Deleted comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
     colors: {
-      transparent: 'transparent',
-      'black': '#000', // checkbox border (with opacity) and hamburger button border (with opacity)
+      'black': '#000',
       'white': '#fff',
       'grey': {
-        100: '#f7f7f7', // Body and checkbox
+        100: '#f7f7f7',
         200: '#ced4da',
         300: '#6c757d',
         400: '#212529'
       },
       'blue': {
-        100: '#cee7f8', // Secondary
-        200: '#104c75', // Primary, button no hover, border cards, border footer, navbar active, menu hamburguesa
-        300: '#0e4163', // Button hover bg
-        400: '#0d3d5e', // Button border
-        500: '#04131d' // Link hover
+        100: '#cee7f8',
+        200: '#104c75',
+        300: '#0e4163',
+        400: '#0d3d5e',
+        500: '#04131d'
       },
       'dirty-blue': '#88a6ba'
     },
